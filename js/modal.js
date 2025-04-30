4e42ec1f33d50fd1d9bb2eb14fbfe1f93c450781
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-04-30 16:35:21 +0300

    add mobile menu open-close js

--- js/modal.js
+++ js/modal.js
@@ -6,13 +6,24 @@
     closeModalBtn: document.querySelector("[data-modal-close]"),
     // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector("[data-modal]"),
+
+    openMobileBtn: document.querySelector("[data-mobile-modal-open]"),
+    closeMobileBtn: document.querySelector("[data-mobile-modal-close]"),
+    mobile: document.querySelector("[data-mobile]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
+  refs.openMobileBtn.addEventListener("click", toggleMobile);
+  refs.closeMobileBtn.addEventListener("click", toggleMobile);
+
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle("is-open");
+  }
+
+  function toggleMobile() {
+    refs.mobile.classList.toggle("is-open");
   }
 })();
