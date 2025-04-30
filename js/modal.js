2be1b90437ef637945515b9dd0ce2827cedd416c
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-04-30 23:34:47 +0300

    add portfolio section

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,6 @@
 (() => {
   const refs = {
+    //Desktop
     // Додати атрибут data-modal-open на кнопку відкриття
     openModalBtn: document.querySelector("[data-modal-open]"),
     // Додати атрибут data-modal-close на кнопку закриття
@@ -7,22 +8,27 @@
     // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector("[data-modal]"),
 
+    //Mobile
     openMobileBtn: document.querySelector("[data-mobile-open]"),
     closeMobileBtn: document.querySelector("[data-mobile-close]"),
     mobile: document.querySelector("[data-mobile]"),
   };
 
+  //Desktop
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
+  //Mobile
   refs.openMobileBtn.addEventListener("click", toggleMobile);
   refs.closeMobileBtn.addEventListener("click", toggleMobile);
 
+  //Desktop
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle("is-open");
   }
 
+  //Mobile
   function toggleMobile() {
     refs.mobile.classList.toggle("is-open");
   }
